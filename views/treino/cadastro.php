6d0241b61d60e04d7929e88dff8868c2d14290bf
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-09-30 11:07:06 -0300

    initial.rogerio - treino - cadastro.php

---
+++ views/treino/cadastro.php
@@ -0,0 +1,14 @@
+<?php include('../../includes/header.php')?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Treinos</title>
+</head>
+<body>
+    
+</body>
+<?php include('../../includes/footer.php')?>
+</html>
