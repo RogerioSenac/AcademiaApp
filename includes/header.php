9cb4a1f4747c605ad6f2cd46c09650c5c1d4cbbb
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-10-10 11:09:31 -0300

    refacture.rogerio - geral

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Academia</a>
+        <a class="navbar-brand" href="#">Academia Whey Proteic</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#" ><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav-item">
