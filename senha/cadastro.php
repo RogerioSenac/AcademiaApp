0cd14fb0e46fa566f447c027f1f3cec3579e1a7e
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-10-14 09:34:43 -0300

    refacture.rogerio - geral

--- senha/cadastro.php
+++ senha/cadastro.php
@@ -1,12 +1,22 @@
 <?php
 include('../bd/conexao.php');
 
+class {
+
+}
+
+private $db;
+public function __construct()
+{
+    $this->db = Conexao::novaConexao();
+}
+
 $nome = $_POST['nomeCompleto'];
 $email = $_POST['email'];
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$cadUser = $instance->prepare ("INSERT INTO acesso (nomeUsuario, emailUsuario, usuario, senhaUsuario) VALUES (?,?,?,?)");
+$cadUser = $this->db->prepare ("INSERT INTO acesso (nomeUsuario, emailUsuario, usuario, senhaUsuario) VALUES (?,?,?,?)");
 $cadUser->execute([$nome, $email, $usuario, $senha]);
 
 header("Location: DashAcesso.php");
