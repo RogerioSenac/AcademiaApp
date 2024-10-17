8e68c8980732d90931be54a83b166bf84b54281b
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-10-17 09:36:37 -0300

    refacture - index.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,18 +4,85 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css.style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title></title>
+    <title>Presbiteriana Jacupiranga</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS (para ícones) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <!-- CSS Personalizado -->
+    <link rel="stylesheet" href="Assets/css/styles.css">
+    <!-- <style>
+        footer {
+            background-color: #f8f9fa;
+            padding: 20px 0;
+        }
+
+        .footer-text {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .card-body-1,
+        .card-body-2 {
+            padding: 10px; /* Padding de 10px */
+            text-align: left; /* Alinhamento à esquerda */
+        }
+
+        /* .card-body-3 {
+            padding: 10px; /* Padding de 10px */
+            /*text-align: right; /* Alinhamento à direita */
+        } */
+
+        .card-group {
+            display: flex;
+            justify-content: space-between; /* Espaço entre os cards */
+        }
+
+        .card {
+            flex: 1; /* Cada card ocupa a mesma largura */
+            margin: 0 10px; /* Margem horizontal para espaçamento */
+        }
+    </style> -->
 </head>
 
 <body>
-    <footer class="footer mt-auto py-3 bg-light">
-        <div class="container text-center">
-            <span class="text-muted">Academia - Todos os direitos reservados a DevelopersRGT</span>
+    <!-- Rodapé -->
+    <footer class="rodape">
+        <div class="card-group">
+                <div class="card-body-1">
+                    <h3>Venha nos visitar.</h3>
+                    <ul>
+                        <li><i class="fas fa-map-marker-alt"></i> Rua Januário Lisboa, 82</br> Vila Elias,
+                            Jacupiranga, SP</li>
+                        <li><i class="fas fa-envelope"></i> acquavidacademia@gmail.com</li>
+                    </ul>
+                </div>
+        </div>
+
+        <div class="card-group">
+                <div class="card-body-2">
+                    <h3>Siga-nos nas redes sociais</h3>
+                    <div class="social-icons">
+                        <a href="https://www.facebook.com/AcademiaAcquaVida/" styles="--color:#ddd" class="facebook"
+                            target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+                        <a href="https://www.instagram.com/acquavidaacademia/" class="instagram" target="_blank"><i
+                                class="fab fa-instagram fa-2x"></i></a>
+                        <a href="https://api.whatsapp.com/send?phone=5513981555763&text=Ol%C3%A1%2C+gostaria+de+obter+mais+informa%C3%A7%C3%B5es+sobre+a+academia&fbclid=PAZXh0bgNhZW0CMTEAAaaD4bGX0MgJgMiUAZTPruyeFsMAQZZ_dJtaAOyAzoC61wbZNw447Y9Yd0A_aem_vDLou7RDSd7NyyaoSkooUA"
+                            class="whatsapp" target="_blank"><i class="fab fa-whatsapp fa-2x"></i></a>
+                        <a href="mailto:acquavidacademia@gmail.com" class="email" target="_blank"><i
+                                class="fas fa-envelope"></i></a>
+                    </div>
+                </div>
         </div>
     </footer>
+
+    <!-- Bootstrap JS e Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <!-- JS Personalizado -->
+    <script src="Assets/js/main.js"></script>
+
+
 </body>
 
 </html>
